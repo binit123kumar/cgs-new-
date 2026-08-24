3b01500bb171e72b9b1c736dce3bb390dc155753
binit kumar <binit491@gmail.com>
2026-08-24 21:00:29 +0530

    Avoid HTTPS redirect warning on Render

--- Backend/backend/CGS.CMS.API/Program.cs
+++ Backend/backend/CGS.CMS.API/Program.cs
@@ -128,9 +128,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-if (!app.Environment.IsDevelopment())
-    app.UseHttpsRedirection();
-
 app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
